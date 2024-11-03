4e44c0dfcb50c44adc543a5b279023a5d3e75c69
Pixel <scott.blatt.0b10@gmail.com>
2024-11-02 23:31:46 -0500

    updates

--- 02-core-cfg.sh
+++ 02-core-cfg.sh
@@ -30,27 +30,11 @@ case "$HOSTNAME" in
 esac
 aug set '/files/etc/kernel/cmdline/rw'
 aug set '/files/etc/kernel/cmdline/loglevel' 4
-
-cat > "$(CreateFile /etc/mkinitcpio.d/linux-zen.preset)" << EOF
-ALL_config="/etc/mkinitcpio.conf"
-ALL_kver="/boot/vmlinuz-linux-zen"
-PRESETS=('default' 'fallback')
-default_uki="/boot/archlinux-zen.efi"
-fallback_uki="/boot/archlinux-zen-fallback.efi"
-fallback_options="-z cat -S autodetect"
-EOF
+aug set '/files/etc/kernel/cmdline/init' /usr/lib/systemd/systemd
 
 cat > "$(CreateFile /etc/doas.conf 400)" <<EOF
 permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel
 permit nopass :wheel as root cmd /usr/bin/aconfmgr
-EOF
-
-cat > "$(CreateFile /etc/mkinitcpio.conf)" << EOF
-BINARIES=(btrfs)
-HOOKS=(systemd autodetect plymouth microcode modconf kms keyboard keymap sd-vconsole block filesystems)
-COMPRESSION="xz"
-COMPRESSION_OPTIONS=("-9e")
-MODULES_DECOMPRESS="no"
 EOF
 
 file="$(GetPackageOriginalFile glibc /etc/locale.gen)"
