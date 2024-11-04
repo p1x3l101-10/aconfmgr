07c95ae5be9557f390450038d77e451557c40f95
Pixel <scott.blatt.0b10@gmail.com>
2024-11-04 09:29:00 -0600

    silencing

--- 09-plymouth.sh
+++ 09-plymouth.sh
@@ -1,5 +1,10 @@
-aug set '/files/etc/kernel/cmdline/splash'
-aug set '/files/etc/kernel/cmdline/quiet'
-aug set '/files/etc/kernel/cmdline/plymouth.use-simpledrm'
-aug set '/files/etc/kernel/cmdline/plymouth.nolog'
-aug set '/files/etc/kernel/cmdline/loglevel' 0 # Silence kernel when using plymouth
+f='/files/etc/kernel/cmdline'
+aug set "$f/splash"
+aug set "$f/quiet"
+aug set "$f/plymouth.use-simpledrm"
+aug set "$f/plymouth.nolog"
+aug set "$f/loglevel" 3 # Silence kernel when using plymouth
+aug set "$f/systemd.show_status" auto
+aug set "$f/rd.udev.log_level" 3
+
+echo "kernel.printk = 3 3 3 3" > "$(CreateFile /etc/sysctl.d/20-quiet-printk.conf)"
